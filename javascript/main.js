cf0124623304410328e3a990a05c6d71907d0fae
Luwin Changco <luwinchangco@gmail.com>
2017-06-16 10:48:20 -0400

    Puggle Tea Case Study Added

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+// Scroll up button
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
